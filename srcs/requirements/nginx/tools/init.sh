cbccdf876af219bbc78d4242c3d241113b82d8b3
Angele Barzilai <abarzila@z3r3p5.42lyon.fr>
2026-05-26 15:03:53 +0200

    working on the nginx config

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,20 +1,20 @@
-# !/bin/sh
+# # !/bin/sh
 
-echo NGINX_SCRIPT
+# echo NGINX_SCRIPT
 
-if [ ! -f /etc/nginx/ssl/cert.pem ]; then
-    mkdir -p /etc/nginx/ssl
+# if [ ! -f /etc/nginx/ssl/cert.pem ]; then
+#     mkdir -p /etc/nginx/ssl
 
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/nginx/ssl/key.pem \
-        -out /etc/nginx/ssl/cert.pem \
-        -subj "/C=CO/ST=REG/L=City/O=42/CN=${DOMAIN_NAME}"
+#     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#         -keyout /etc/nginx/ssl/key.pem \
+#         -out /etc/nginx/ssl/cert.pem \
+#         -subj "/C=CO/ST=REG/L=City/O=42/CN=${DOMAIN_NAME}"
 
-    sed -i "s/server_name localhost/server_name ${DOMAIN_NAME}/g" /etc/nginx/http.d/nginx.conf
+#     sed -i "s/server_name localhost/server_name ${DOMAIN_NAME}/g" /etc/nginx/http.d/nginx.conf
 
-    # cp 404.html /var/www/html/404.html
-fi
+#     # cp 404.html /var/www/html/404.html
+# fi
 
-exec nginx -g "daemon off;"
+# exec nginx -g "daemon off;"
 
-# THIS IS NOT MY CODE. I COPY PASTED IT FOR A TEST
+# # THIS IS NOT MY CODE. I COPY PASTED IT FOR A TEST
