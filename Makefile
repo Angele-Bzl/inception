a217d364731c1167753e07a2e042b7cbc13725b7
Angele Barzilai <abarzila@z3r3p5.42lyon.fr>
2026-06-01 11:31:34 +0200

    dockerfile mariadb is ok ?

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ all:
 	docker compose -f srcs/docker-compose.yml up -d --build --no-recreate 
 # -d --build --no-recreate ?
 #
-#
+build-%:
+	docker compose up --build -d $*
+
+$(TERM) -e sh -c "docker exec -it $*"
--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,11 @@
 # !/bin/sh
 
-	sudo systemctl status mariadb \
-    && sudo systemctl start mariadb \
-    && mariadb -u root -p
+	# systemd status mariadb \
+    # && systemd start mariadb \
+    # && mariadb -u root -p
+
+
+
+# Mariadb daemon in foreground
+exec mariadb --user=mysql --console
+# il faut exec mariadb
