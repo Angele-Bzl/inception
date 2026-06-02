750b5ca93671f4994e32fabe308d7459cad2c89d
Angele Barzilai <abarzila@z3r3p5.42lyon.fr>
2026-06-02 16:32:04 +0200

    small fix mariadb

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,7 +4,7 @@
     # && systemd start mariadb \
     # && mariadb -u root -p
 
-if [ ! -d "/var/lib/mysql" ]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     mariadbd --user=mysql --skip-networking &
@@ -14,10 +14,10 @@ if [ ! -d "/var/lib/mysql" ]; then
         sleep 1
     done
 
-    mariadb -u root -e "ALTER USER 'rooter@localhost' 'IDENTIFIED BY ${MYSQL_ROOT_PASSWORD};"
-    mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \` ${MYSQL_DATABASE}\`;"
+    mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER_ADMIN}'@'%' IDENTIFIED BY'${MY_SQL_PASSWORD_ADMIN}'"
+    mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER_ADMIN}'@'%' IDENTIFIED BY '${MY_SQL_PASSWORD_ADMIN}';"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER_ADMIN}'@'%';"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
