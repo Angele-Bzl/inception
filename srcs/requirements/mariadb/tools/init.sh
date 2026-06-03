2371613ecd1217b201aa35b8223c52be3babda87
Angele Barzilai <abarzila@z1r4p5.42lyon.fr>
2026-06-03 16:31:26 +0200

    wordpress dockerfile and inti

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -16,7 +16,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
     mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-    mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    # mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER_ADMIN}'@'%' IDENTIFIED BY '${MY_SQL_PASSWORD_ADMIN}';"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER_ADMIN}'@'%';"
     mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -3,7 +3,7 @@
 if  [ ! -f "/var/www/html/wp-config.php" ]; then
     wget https://wordpress.org/latest.tar.gz
     tar -xzf latest.tar.gz
-    mv wordpress/* /var/www/thml/
+    mv wordpress/* /var/www/html/
     rm -rf wordpress latest.tar.gz
 
     until mariadb-admin ping -h mariadb --silent; do
@@ -11,8 +11,33 @@ if  [ ! -f "/var/www/html/wp-config.php" ]; then
         sleep 1
     done
     
-    # Configure and install WordPress here via WP-CLI or manual scripts...
-    # (This is where you set up your login.42.fr URL and non-admin users)
+    # Configures the DB connection details
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER_ADMIN}" \
+        --dbpass="${MYSQL_PASSWORD_ADMIN}" \
+        --dbhost="mariadb" \
+        --allow-root
+
+    # Installs WP and sets up the Admin (Make sure $WP_ADMIN_USER doesn't contain 'admin'!)
+    wp core install \
+        --url="${WP_URL}" \
+        --title="Inception WordPress" \
+        --admin_user="${WP_ADMIN_USER}" \
+        --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" \
+        --allow-root
+
+    # Creates the mandatory second, regular user
+    wp user create \
+        "${WP_USER}" \
+        "${WP_USER_EMAIL}" \
+        --user_pass="${WP_USER_PASSWORD}" \
+        --role=author \
+        --allow-root
+
+    chown -R www-data:www-data /var/www/html
+
 fi
 
 exec php-fpm8.2 -F
